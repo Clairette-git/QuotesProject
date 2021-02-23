0335f7c02bf4c1ec235ce3b2d88feb6340e7c45d
Clairette-git <marcletty12@gmail.com>
2021-02-24 01:44:01 +0200

    update styles file

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -23,6 +24,11 @@ module.exports = function (config) {
     },
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/hello'),
